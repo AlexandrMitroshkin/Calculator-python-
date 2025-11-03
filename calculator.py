76b0d7f80e8845fe62a0b5f8027954f0302821ca
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2025-11-03 17:02:10 +0300

    plus_minus_differece_multiplication_are_ready

--- calculator.py
+++ calculator.py
@@ -1,77 +1,119 @@
 user_input = input("Введите выражение: ")
-common_expression = user_input.split()
-expression = []
-result = 0
-indicator = 0
+common_expr = user_input.split()
+expr = []
 
-def is_action(x):
-    if type(x) == str:
-        return True
-    else :
-        return False
-    
-for i in common_expression:
+for i in common_expr:
     if i.lstrip('-').isdigit():
-        expression.append(int(i))
-    elif i == "+" or i == "-":
-        expression.append(i)
+        expr.append(int(i))
+    elif i.lstrip('(').isdigit() or i.rstrip(')').isdigit():
+        if i == '(' or i == ')':
+            expr.append(i)
+        if len(i) > 1:
+            if i[0] == '(':
+                expr.append('(')
+                expr.append(int(i[1:]))
+            elif i[-1] == ')':
+                expr.append(int(i[:-1]))
+                expr.append(')')
+    elif i == "+" or i == "-" or i == "*" or i == "/" or i =='(' or i == ')':
+        expr.append(i)
 
-def summa(action_position, list_expression):
-    left = list_expression[action_position-1]
-    right = list_expression[action_position+1]
-    list_expression[action_position+1] = left + right
-    list_expression.remove(list_expression[action_position])
-    list_expression.remove(left)
+                                                  
+def summ(act_index, list_expr):
+    left = list_expr[act_index-1]
+    right = list_expr[act_index+1]   
+    list_expr[act_index+1] = left + right
+    del list_expr[act_index]
+    del list_expr[list_expr.index(left)]
+
+def diff(act_index, list_expr):
+    left = list_expr[act_index-1]
+    right = list_expr[act_index+1]
+    list_expr[act_index+1] = left - right
+    del list_expr[act_index]
+    del list_expr[list_expr.index(left)]
+
+def mult(act_index, list_expr):
+    left = list_expr[act_index-1]
+    right = list_expr[act_index+1]
+    list_expr[act_index+1] = left * right
+    del list_expr[act_index]
+    del list_expr[list_expr.index(left)]
+
+def div(act_index, list_expr):
+    left = list_expr[act_index-1]
+    right = list_expr[act_index+1]
+    list_expr[act_index+1] = left / right
+    del list_expr[act_index]
+    del list_expr[list_expr.index(left)]
 
 
-def difference(action_position, list_expression):
-    left = list_expression[action_position-1]
-    right = list_expression[action_position+1]
-    list_expression[action_position+1] = left - right
-    list_expression.remove(list_expression[action_position])
-    list_expression.remove(left)
-
-
-def recyrsia(counter , list_expression):
-    global result
-    global indicator
-    if counter == 1:
-        if list_expression[1] == '+':
-            result += list_expression[0] + list_expression[2]
-            indicator += 1
-        elif expression[1] == '-':
-            result += list_expression[0] - list_expression[2]
-            indicator += 1
-    elif counter != 1:
-        for i in list_expression:
-            if is_action(i):
-                if i == "+":
-                    summa(list_expression.index(i), list_expression)
-                    counter -= 1
-                    recyrsia(counter , list_expression)
-                elif i == "-":
-                    difference(list_expression.index(i), list_expression)
-                    counter -= 1
-                    recyrsia(counter , list_expression)
-    return result
-
-def operation():
-    global result
-    global indicator
-    for i in expression:
-        if len(expression) == 1 and type(i) != str:
-            result =  expression[0]
-            return result
-        elif len(expression) == 1 and type(i) == str:
-            return "Неправильный синтаксис, попробуйте еще раз"
-        elif len(expression) != 1:
-            counter_actions = 0
-            for item in expression:
-                if is_action(item):
-                    counter_actions += 1
-            if indicator == 0:
-                recyrsia(counter_actions , expression)
-            else:
-                return result
+def operation(expr):
+    if len(expr) == 1:
+        if type(expr[0]) == int or float:
+            return expr[0] 
+        else:
+            return "Ошибка: одиночный оператор"
+        
+    for i in expr:
+        if type(i) == str:
+            if i == "*":
+                mult(expr.index(i), expr)
+                return operation(expr)
+            elif i == "/":
+                div(expr.index(i), expr)
+                return operation(expr)  
             
-print(operation())
+    for item in expr:
+        if type(item) == str:
+            if item == "+":
+                summ(expr.index(item), expr)
+                return operation(expr)
+            elif item == "-":
+                diff(expr.index(item), expr)
+                return operation(expr)
+    return "Ошибка: нет операторов"
+
+# def brace_operation(expr):
+#     brace_dict = {}
+#     newExpr = []
+#     for i in range(len(expr)):
+#         if expr[i] == "(":
+#             if len(brace_dict) == 0:
+#                 brace_dict["br1"] = {
+#                     "status": "untaken",
+#                     "pair": 1,
+#                     "depth": 0,
+#                     "index" : i
+#                 }
+
+def inspection(expr):
+    brece_list = []
+    if len(expr) == 0:
+        print("Пустое выражение")
+        return
+
+    for i in range(len(expr)):
+        if type(expr[i]) == str and expr[i] != "(" and expr[i] != ")":
+            if i == 0 or i == len(expr)-1:
+                print("Ошибка: оператор в начале или конце")
+                return
+            if type(expr[i-1]) != int and expr[i-1] not in ("(", ")") or type(expr[i+1]) != int and expr[i-1] not in ("(", ")"):
+                print("Ошибка: рядом с оператором нет чисел")
+                return
+        if expr[i] == "(":
+            brece_list.append(expr[i])
+        if expr[i] == ")":
+            if len(brece_list) == 0:
+                print("Ошибка несоответствие скобок")
+            else:
+                brece_list.pop()
+    
+    if len(brece_list) == 0:         
+        result = operation(expr)
+        print(result)
+
+    else: 
+        print("Ошибка несоответствие скобок")
+inspection(expr)
+
