f07c89425b90f1f601765051943af5cf400a6474
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2025-11-18 18:52:24 +0300

    so-so ready

--- calculator.py
+++ calculator.py
@@ -3,6 +3,7 @@ common_expr = user_input.split()
 expr = []
 
 for i in common_expr:
+    temporary_list = []
     if i.lstrip('-').isdigit():
         expr.append(int(i))
     elif i.lstrip('(').isdigit() or i.rstrip(')').isdigit():
@@ -10,11 +11,16 @@ for i in common_expr:
             expr.append(i)
         if len(i) > 1:
             if i[0] == '(':
-                expr.append('(')
-                expr.append(int(i[1:]))
+                for item in i:
+                    if item == "(":
+                        expr.append('(')
+                expr.append(int(i.lstrip('(')))
             elif i[-1] == ')':
-                expr.append(int(i[:-1]))
-                expr.append(')')
+                expr.append(int(i.rstrip(')')))
+                for item in i:
+                    if item == ")":
+                        expr.append(')')
+
     elif i == "+" or i == "-" or i == "*" or i == "/" or i =='(' or i == ')':
         expr.append(i)
 
@@ -55,6 +61,9 @@ def operation(expr):
         else:
             return "Ошибка: одиночный оператор"
         
+    if "(" in expr:
+        brace_operation(expr)
+    
     for i in expr:
         if type(i) == str:
             if i == "*":
@@ -74,21 +83,113 @@ def operation(expr):
                 return operation(expr)
     return "Ошибка: нет операторов"
 
-# def brace_operation(expr):
-#     brace_dict = {}
-#     newExpr = []
-#     for i in range(len(expr)):
-#         if expr[i] == "(":
-#             if len(brace_dict) == 0:
-#                 brace_dict["br1"] = {
-#                     "status": "untaken",
-#                     "pair": 1,
-#                     "depth": 0,
-#                     "index" : i
-#                 }
+def brace_operation(expr):
+    brace_dict = {}
+    for i in range(len(expr)):
+        if expr[i] == "(":
+            if len(brace_dict) == 0:
+                brace_dict["br1"] = {
+                    "status": "untaken",
+                    "pair": 1,
+                    "depth": 0,
+                    "index" : i,
+                }
+            elif len(brace_dict) > 0:
+                comparsion_pair = []
+                current_depth = 0
+                for brace in brace_dict.keys():
+                    if brace_dict[brace]["status"] == 'untaken':
+                        current_depth += 1
+                    comparsion_pair.append(brace_dict[brace]["pair"])
+
+                the_biggest_pair = max(comparsion_pair)           
+
+                brace_dict['br' + str(the_biggest_pair + 1)] = {
+                    "status": "untaken",
+                    "pair": the_biggest_pair + 1,
+                    "depth": current_depth,
+                    "index" : i,
+                }    
+
+        if expr[i] == ")":            
+            if len(brace_dict) == 1:
+                brace_dict["br1_cl"] = {
+                    "status": "taken",
+                    "pair": 1,
+                    "depth": 0,
+                    "index" : i,
+                }
+
+                brace_dict["br1"]["status"] = "taken"
+
+            elif len(brace_dict) > 0:
+
+                list_of_pairs = []
+                for item in brace_dict.keys():
+                    if brace_dict[item]["status"] == "untaken":
+                        list_of_pairs.append(brace_dict[item]["pair"])
+                max_untaken_pair = max(list_of_pairs)
+
+                for item in brace_dict.keys():
+                    if brace_dict[item]["pair"] == max_untaken_pair:
+                        pair_open_key = item 
+                        break
+
+                brace_dict["br" + str(max_untaken_pair) + "_cl"] = {
+                    "status": "taken",
+                    "pair": max_untaken_pair,
+                    "depth": brace_dict[pair_open_key]["depth"],
+                    "index" : i,
+                }
+                brace_dict[pair_open_key]["status"] = "taken"
+
+    newExpr = expr
+    depth_list = []
+
+    for i in brace_dict.keys():
+        depth_list.append(brace_dict[i]["depth"])
+
+    while len(depth_list) > 1:
+
+        keys_list = list(brace_dict.keys())
+
+        the_deepest_number = max(depth_list)
+        for action in keys_list: 
+            if brace_dict[action]["depth"] == the_deepest_number:
+                if newExpr[brace_dict[action]['index']] == "(":
+                    open_brace = action
+                else :
+                    close_brace = action
+                    break
+
+        temporary_list = (newExpr[brace_dict[open_brace]['index'] + 1:brace_dict[close_brace]['index']])
+        len_temporary_list = len(temporary_list)
+        newExpr = newExpr[:brace_dict[open_brace]['index']] + newExpr[brace_dict[close_brace]['index'] + 1:]
+        newExpr.insert(brace_dict[open_brace]['index'] , operation(temporary_list))
+
+        brace_open_index = brace_dict[open_brace]['index']
+        brace_close_index = brace_dict[close_brace]['index']
+
+        temporary_counter = 0
+        while temporary_counter  < 2:
+            del depth_list[the_deepest_number]
+            temporary_counter += 1
+        del brace_dict[open_brace]
+        del brace_dict[close_brace]
+        keys_list.remove(open_brace)
+        keys_list.remove(close_brace)
+
+        for brace in keys_list:
+            if brace_dict[brace]['index'] >= brace_open_index:
+                brace_dict[brace]['index'] -= len_temporary_list + 1
+
+    if len(brace_dict) == 0:
+        return(operation(newExpr))
+
 
 def inspection(expr):
     brece_list = []
+    brace_exist = False
     if len(expr) == 0:
         print("Пустое выражение")
         return
@@ -98,10 +199,11 @@ def inspection(expr):
             if i == 0 or i == len(expr)-1:
                 print("Ошибка: оператор в начале или конце")
                 return
-            if type(expr[i-1]) != int and expr[i-1] not in ("(", ")") or type(expr[i+1]) != int and expr[i-1] not in ("(", ")"):
+            if (type(expr[i-1]) != int and expr[i-1] not in ("(", ")")) or (type(expr[i+1]) != int and expr[i+1] not in ("(", ")")):
                 print("Ошибка: рядом с оператором нет чисел")
                 return
         if expr[i] == "(":
+            brace_exist = True
             brece_list.append(expr[i])
         if expr[i] == ")":
             if len(brece_list) == 0:
@@ -109,11 +211,22 @@ def inspection(expr):
             else:
                 brece_list.pop()
     
-    if len(brece_list) == 0:         
-        result = operation(expr)
-        print(result)
-
+    if brace_exist == False:
+        print("Ответ:" , operation(expr))   
+    elif brace_exist == True:
+        print("Ответ:" , brace_operation(expr))
     else: 
         print("Ошибка несоответствие скобок")
+
 inspection(expr)
+
+
+
+
+
+
+
+
+
+
 
